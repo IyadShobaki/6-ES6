e21cb1555981e3e0b8e7231defd2d1d427d48af4
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-25 14:33:18 -0400

    Arrow Functions: Lexical 'this' Keyword

--- script.js
+++ script.js
@@ -117,6 +117,7 @@ console.log(fullName.includes('p'));
 console.log(`${firstName} `.repeat(5));
 */
 
+/*
 // Arrow functions: Basics
 
 const years = [1990, 1965, 1982, 1937];
@@ -143,8 +144,74 @@ ages6 = years.map((el, index) => {
     return `Age element ${index + 1}: ${age}.`;
 });
 console.log(ages6);
+*/
+
+// Arrow functions: Lexical 'this' keyword
+
+// ES5 version
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function () {
+        var self = this;
+        document.querySelector('.green').addEventListener('click', function () {
+           var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+           alert(str); 
+        });
+    }
+}
+
+//box5.clickMe();
+
+// ES6 version
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function () {
+        document.querySelector('.green').addEventListener('click', () => {
+           var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+           alert(str); 
+        });
+    }
+}
+
+box6.clickMe();
+
+// ES6 version not working version 
+const box62 = {
+    color: 'green',
+    position: 1,
+    clickMe: () => {
+        document.querySelector('.green').addEventListener('click', () => {
+           var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+           alert(str); 
+        });
+    }
+}
+
+//box62.clickMe();  // this is undefiend
 
 
+//Another example of arrow function usinf class (function constructor)
+function Person(name) {
+    this.name = name;
+}
+// ES5 version
+Person.prototype.myFriends5 = function (friends) {
+    var arr = friends.map(function (friend) {
+       return this.name + ' is friend with '+ friend; 
+    }.bind(this));
+    console.log(arr);
+}
+var friends = ['Bob', 'Jane', 'Mark'];
+new Person('John').myFriends5(friends);
 
+//ES6 version
+Person.prototype.myFriends6 = function (friends) {
+    const arr = friends.map(friend => `${this.name} is friend with ${friend}`);
+
+    console.log(arr);
+}
+new Person('Mike').myFriends6(friends);
 
 
